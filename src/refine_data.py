b6b81a8b22a4d3018fcc98c0c9da9ea03c252a45
sergio estevao <smestevao11@gmail.com>
2022-10-10 04:29:10 +0100

    removed comments

--- src/refine_data.py
+++ src/refine_data.py
@@ -45,16 +45,8 @@ def getMainAreaTitle(area):
     mainIndex = text.find("<main>")
     areaIndex = text.rfind(area, mainIndex)
 
-    #print("mainIndex: ", mainIndex)
-    #print("areaIndex: ", areaIndex)
-
-    #print("<<<<<<<<<<<<<<<", text[mainIndex:areaIndex])
-
     h2Index = text.rfind("<h2>", mainIndex, areaIndex)
     h2FinalIndex = text.rfind("</h2>", mainIndex, areaIndex)
-
-    #print("h2Index: ", h2Index)
-    #print("h2FinalIndex: ", h2FinalIndex)
 
     result = text[h2Index+4:h2FinalIndex]
 
@@ -97,14 +89,8 @@ def main():
                 continue
 
             if tag not in areaDict.keys():
-                # if tagsDict[field] == "Computer Science":
-                #     areaDict[spec] = "Computer Science"
-                # else:
-                #     areaDict[spec] = getMainAreaTitle(tagsDict[spec])
                 areaDict[tag] = getMainAreaTitle(tag)
 
-
-            
 
             areaTitle = areaDict[tag]
             fieldTitle = tagsDict[field]
@@ -114,17 +100,12 @@ def main():
                 newTags[areaTitle] = {}
             
 
-            # print("areaTitle: ", areaTitle)
-            # print("fieldTitle: ", fieldTitle)
-            # print("specTitle: ", specTitle)
-            # print(newTags)
             if fieldTitle not in newTags[areaTitle].keys():    
                 newTags[areaTitle][fieldTitle] = [specTitle]
             elif specTitle not in newTags[areaTitle][fieldTitle]:
                 newTags[areaTitle][fieldTitle].append(specTitle)
 
-            # print(newTags)
-            # print("================")
+
             
             
         paper['tags'] = newTags
@@ -135,73 +116,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-    # with open('./data/clean_data.json', 'r') as dataset:
-    #     papers = json.loads(dataset.read())
-    
-    # updatedTags = {}
-    # tagsDict = {}
-    # areaDict = {}
-
-    # for paper in papers:
-
-    #     if paper['title'] != "A Note on Zipf's Law, Natural Languages, and Noncoding DNA regions":
-    #         continue
-
-    #     urlTags = paper["tags"]
-    #     newTags = {}
-        
-    #     for tag in urlTags:
-
-    #         if tag not in updatedTags.keys():
-    #             updatedTags[tag] = getUpdatedTag(tag)
-    #         tag = updatedTags[tag]
-
-    #         tokens = tag.split(".")
-    #         field = tokens[0]
-    #         if len(tokens) == 1: spec = tokens[0]
-    #         else: spec = tokens[1]
-
-    #         if field not in tagsDict.keys():
-    #             tagsDict[field] = getTitle(field)
-
-    #         if spec not in tagsDict.keys():
-    #             tagsDict[spec] = getTitle(tag)
-
-    #         if ("pastweek" in tagsDict[field]) or ("pastweek" in tagsDict[spec]):
-    #             continue
-
-    #         if tag not in areaDict.keys():
-    #             # if tagsDict[field] == "Computer Science":
-    #             #     areaDict[spec] = "Computer Science"
-    #             # else:
-    #             #     areaDict[spec] = getMainAreaTitle(tagsDict[spec])
-    #             areaDict[tag] = getMainAreaTitle(tag)
-
-
-            
-
-    #         areaTitle = areaDict[tag]
-    #         fieldTitle = tagsDict[field]
-    #         specTitle = tagsDict[spec]
-
-    #         if areaTitle not in newTags.keys():
-    #             newTags[areaTitle] = {}
-            
-
-    #         # print("areaTitle: ", areaTitle)
-    #         # print("fieldTitle: ", fieldTitle)
-    #         # print("specTitle: ", specTitle)
-    #         # print(newTags)
-    #         if fieldTitle not in newTags[areaTitle].keys():    
-    #             newTags[areaTitle][fieldTitle] = [specTitle]
-    #         elif specTitle not in newTags[areaTitle][fieldTitle]:
-    #             newTags[areaTitle][fieldTitle].append(specTitle)
-
-    #         # print(newTags)
-    #         # print("================")
-            
-            
-    #     paper['tags'] = newTags
-
-    # file = open("./data/refined_data.json", "w+")
-    # json.dump(papers, file, indent=2)
