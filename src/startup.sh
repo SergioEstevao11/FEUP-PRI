8b4f74b410f58a9e6b30d51494d2275cc224b3d8
Bibs <mbrlopes.santos@gmail.com>
2022-11-08 11:03:47 +0000

    Solr

---
+++ src/startup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+precreate-core papers
+
+# Start Solr in background mode so we can use the API to upload the schema
+solr start
+
+# Schema definition via API
+curl -X POST -H 'Content-type:application/json' \
+    --data-binary @/data/simple_schema.json \
+    http://localhost:8983/solr/papers/schema
+
+# Populate collection
+bin/post -c papers /data/refined_data.json
+
+# Restart in foreground mode so we can access the interface
+solr restart -f
