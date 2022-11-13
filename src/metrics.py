0ed4702e06e881b41da00126b9bf46f05c3f6a83
sergio estevao <smestevao11@gmail.com>
2022-11-13 12:52:44 +0000

    feat: all queries recall-precision graph

--- src/metrics.py
+++ src/metrics.py
@@ -67,13 +67,51 @@ def schema_evalution():
     plt.legend()
     plt.savefig("./queries/allqueries.pdf")
 
+    precision_recall_match = []
+    for j in range(len(queries)):
+        precision_values = [precision(results[j], relevants[j][:i]) for i in range(1, len(relevants) + 1)]
+        recall_values = [recall(results[j], relevants[j][:i]) for i in range(1, len(relevants) + 1)]
+        precision_recall_match.append( {k: v for k,v in zip(recall_values, precision_values)})
+        print("og_recall: ", recall_values)
+
+        # Extend recall_values to include traditional steps for a better curve (0.1, 0.2 ...)
+        recall_values.extend([step for step in np.arange(0.1, 1.1, 0.1) if step not in recall_values])
+        recall_values = sorted(set(recall_values))
+
+        # Extend matching dict to include these new intermediate steps
+        for idx, step in enumerate(recall_values):
+            if step not in precision_recall_match[j]:
+                if recall_values[idx-1] in precision_recall_match[j]:
+                    precision_recall_match[j][step] = precision_recall_match[j][recall_values[idx-1]]
+                else:
+                    precision_recall_match[j][step] = precision_recall_match[j][recall_values[idx+1]]
+        
+
+    frst = precision_recall_match[0]
+    df = pd.DataFrame.from_dict(frst, orient='index')
+    df.columns = ["precision"]
+    df["recall"] = df.index
+    print("df: \n", df)
+    ax = \
+    df.plot.line(x='recall', y='precision', markersize=3, style='-o', label="Q1", figsize=(9, 6))
+
+    for idx in range(1, len(precision_recall_match)):
+        df = pd.DataFrame.from_dict(precision_recall_match[idx], orient='index')
+        df.columns = ["precision"]
+        df["recall"] = df.index
+        df.plot.line(x='recall', y='precision', markersize=3, style='-o', label="Q"+str(idx+1), ax=ax)
 
 
-def main():
-    """schema evalution"""
-    # schema_evalution()
+    plt.title("Recall-precision graphs")
+    plt.xlabel("Recall (R@n)")
+    plt.ylabel("Precision (P@n)")
+    plt.xlim([-0.05, 1.05])
+    plt.ylim([-0.05, 1.05])
+    plt.grid()
+    plt.legend(loc="lower right", ncol=7)
+    plt.savefig('./queries/all-recall-precision.pdf', bbox_inches='tight')
 
-    """query evalution"""
+def query_evalution():
     result = requests.get(QUERY_URL).json()['response']['docs']
     result = list(map(lambda x: x['id'], result))
     relevants = list(map(lambda el: el.rstrip(), open("./queries/" + QUERY_ID + "/relevants.txt").readlines()))
@@ -104,9 +142,12 @@ def main():
     print("recall values: ", recall_values)
     print("f1 values: ", f1_values)
 
-
-
     plot_precision_recall_curve([precision_recall_match.get(r) for r in recall_values], recall_values, title="Precision-Recall curve")
+
+
+def main():
+    schema_evalution()
+    #query_evalution()
 
 
 
