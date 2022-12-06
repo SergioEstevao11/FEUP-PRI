9c2102750ec633a1fbd62d096febd36ffe950a46
sergio estevao <smestevao11@gmail.com>
2022-12-06 15:00:31 +0000

    feat: added react proj

---
+++ src/scihub-light/src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+import Alert from 'react-bootstrap/Alert';
+
+
+function App() {
+  return (
+    <>
+      {[
+        'primary',
+        'secondary',
+        'success',
+        'danger',
+        'warning',
+        'info',
+        'light',
+        'dark',
+      ].map((variant) => (
+        <Alert key={variant} variant={variant}>
+          This is a {variant} alert with{' '}
+          <Alert.Link href="#">an example link</Alert.Link>. Give it a click if
+          you like.
+        </Alert>
+      ))}
+    </>
+  );
+}
+
+export default App;
