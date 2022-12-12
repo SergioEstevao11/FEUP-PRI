229178f8b6868ed1aac58cb3417a1b92891a4654
Bibs <mbrlopes.santos@gmail.com>
2022-12-12 23:42:49 +0000

    Solr backend

--- src/search/papers/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import Paper
-
-class PaperAdmin(admin.ModelAdmin):
-    list_display = ("link", "summary", "title", "authors", "date", "areas", "fields", "subjects")
-
-admin.site.register(Paper, PaperAdmin)
-# Register your models here.
